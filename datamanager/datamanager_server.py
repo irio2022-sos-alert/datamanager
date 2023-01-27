be8672e7e6c704c568b43012a89a4e1af5f33eed
macsyg <macieks1803@gmail.com>
2023-01-27 14:45:56 +0100

    communication test 1

--- datamanager/datamanager_server.py
+++ datamanager/datamanager_server.py
@@ -1,87 +1,156 @@
 import logging
 from concurrent import futures
-import asyncio
+
+from threading import Thread, Event
+import time
+
+import os
+import json
 
 import grpc
 import datamanager_pb2
 import datamanager_pb2_grpc
 
+from db import init_connection_pool, migrate_db
+from models import Services
+from sqlmodel import Session
+
 from google.cloud import pubsub_v1
 
-from threading import Thread, Event
-import json
-import time
-
-import os
+config = {}
 
 # Inherit from example_pb2_grpc.ExampleServiceServicer
 # ExampleServiceServicer is the server-side artifact.
 class DataManager(datamanager_pb2_grpc.DataManagerServicer): 
-
-    def __init__(self) -> None:
-        self.publisher = pubsub_v1.PublisherClient()
-        self.config = parse_config()
-        self.topic = self.create_topic()
-    
     def ChangeConfig(self, 
                     request: datamanager_pb2.ServiceConfig, 
-                    context: grpc.ServicerContext):
+                    _context: grpc.ServicerContext):
 
-        key = request.name
-        self.config[key]["event"].set()
+        # with Session(engine) as session:
+        #     name = request.name
+        #     service = session.query(Services).get(name)
 
-        service = {
-            "name": request.name,
-            "url": request.url,
-            "frequency": request.frequency,
-            "alerting_window": request.alerting_window,
-            "allowed_resp_time": request.allowed_resp_time,
-            "phone_number": request.phone_number,
-            "email": request.email
-        }
+        #     service.url = request.url
+        #     service.frequency = request.frequency
+        #     service.alerting_window = request.name,
+        #     service.allowed_resp_time = request.name
 
-        event = Event()
-        thread = Thread(target=run_in_cycle, args=(request.name, request.frequency, event))
-        self.config[request.name] = {
-            "config": service,
-            "thread_executing": thread,
-            "event": event
-        }
-        thread.start()
+        #     session.add(service)
+        #     session.commit()
+
+        if not config[request.name]["enabled"]:
+            thread = Thread(target=run_in_cycle, args=(request.name))
+            thread.start()
+
+        config[request.name]["url"] = request.url
+        config[request.name]["frequency"] = request.frequency
+        config[request.name]["enabled"] = True
 
         return datamanager_pb2.ResponseMsg(result="okay")
 
     def StopService(self, 
                     request: datamanager_pb2.ServiceName, 
-                    context: grpc.ServicerContext):
+                    _context: grpc.ServicerContext):
         
-        key = request.name
-        self.config[key]["event"].set()
+        # with Session(engine) as session:
+        #     name = request.name
+        #     service = session.query(Services).get(name)
+
+        #     session.delete(service)
+        #     session.commit()
+
+        config[request.name]["enabled"] = False
 
         return datamanager_pb2.ResponseMsg(result="stopped")
 
-    def create_topic(self) -> None:
-        """Create a new Pub/Sub topic."""
-        # [START pubsub_quickstart_create_topic]
-        # [START pubsub_create_topic]
-        project_id = "turing-terminus-374215"
-        topic_id = "datamanager-dataretriever-communication"
+# def run_in_cycle(name):
+#     while True:
+#         with Session(engine) as session:
+#             service = session.query(Services).get(name)
+#             if service is None:
+#                 logging.info(
+#                     f"Invalid confirmation request for {name}, no such service name"
+#                 )
+#                 break
+#             else:
+#                 publisher = pubsub_v1.PublisherClient()
+#                 project_id = os.getenv("PROJECT_ID")
+#                 topic_id = os.getenv("TOPIC_ID")
+#                 topic_path = publisher.topic_path(project_id, topic_id)
 
-        topic_path = self.publisher.topic_path(project_id, topic_id)
+#                 record = {
+#                     'url': service.url,
+#                 }
 
-        try:
-            maybe_topic = self.publisher.get_topic(request={"topic": topic_path})
-            print(f"Topic already exists")
-            return maybe_topic.name
-        except:
-            topic = self.publisher.create_topic(request={"name": topic_path})
-            print(f"Created topic: {topic.name}")
-            return topic.name
+#                 data = json.dumps(record).encode("utf-8")
+#                 future = publisher.publish(topic_path, data)
+#                 logging.info(f'published message id {future.result()}')
 
-        # [END pubsub_quickstart_create_topic]
-        # [END pubsub_create_topic]
+#                 logging.info("HELLO FROM %s", service.name)
+#                 time.sleep(service.frequency)
+
+def run_in_cycle(name):
+    while True:
+        if not config[name]["enabled"]:
+            break
+        publisher = pubsub_v1.PublisherClient()
+        project_id = os.getenv("PROJECT_ID")
+        topic_id = os.getenv("TOPIC_ID")
+        topic_path = publisher.topic_path(project_id, topic_id)
+
+        record = {
+            'url': config[name]["url"],
+        }
+
+        data = json.dumps(record).encode("utf-8")
+        future = publisher.publish(topic_path, data)
+        logging.info(f'published message id {future.result()}')
+
+        logging.info("HELLO FROM %s", name)
+        time.sleep(config[name]["frequency"])
+
+# def init_db():
+#     global engine
+#     engine = init_connection_pool()
+#     migrate_db(engine)
+
+
+# def register_service(config):
+#     with Session(engine) as session:
+#         session.add(
+#             Services(
+#                 name = config["name"],
+#                 url = config["url"],
+#                 frequency = config["frequency"],
+#                 alerting_window = config["alerting_window"],
+#                 allowed_resp_time = config["allowed_resp_time"]
+#             )
+#         )
+#         session.commit()
+
+
+def parse_config() -> None:
+    data = ""
+    with open("config.json") as config_file:
+        data = json.load(config_file)
+
+    # for service in data:
+    #     register_service(service)
+
+    for service in data:
+        config[service["name"]] = {
+            "url": service["url"], 
+            "frequency": service["frequency"],
+            "enabled": True
+        }
+
+    for service in data:
+        thread = Thread(target=run_in_cycle, args=(service["name"]))
+        thread.start()
+
 
 def serve(port) -> None:
+    config = parse_config()
     bind_address = f"[::]:{port}"
     server = grpc.server(futures.ThreadPoolExecutor())
     datamanager_pb2_grpc.add_DataManagerServicer_to_server(
@@ -93,32 +162,6 @@ def serve(port) -> None:
     logging.info("Listening on port %s.", port)
     server.wait_for_termination()
 
-def run_in_cycle(name, interval, event):
-    # perform task in iterations
-    while True:
-        time.sleep(interval)
-        logging.info("HELLO FROM %s", name)
-        if event.is_set():
-            break
-
-def parse_config() -> dict:
-    dic = {}
-    data = ""
-    with open("config.json") as config_file:
-        data = json.load(config_file)
-
-
-    for service in data:
-        event = Event()
-        thread = Thread(target=run_in_cycle, args=(service["name"], service["frequency"], event))
-        dic[service["name"]] = {
-            "config": service,
-            "thread_executing": thread,
-            "event": event
-        }
-        thread.start()
-
-    return dic
 
 if __name__ == "__main__":
     port = os.environ.get("PORT", "50051")
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,10 +2,12 @@
 
 readonly app_name="$1"
 readonly image_name="$2"
+readonly env="$3"
 
 gcloud run deploy $app_name \
 --image $image_name \
 --region europe-north1 \
 --platform managed \
 --allow-unauthenticated \
+--update-env-vars $env \
 --no-cpu-throttling
