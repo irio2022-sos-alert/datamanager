dcb67099792c25490dcc95a44e2c66b9d7108d9e
macsyg <macieks1803@gmail.com>
2023-01-28 23:28:43 +0100

    integration test 5

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,14 +2,13 @@
 
 readonly app_name="$1"
 readonly image_name="$2"
-readonly env="$3"
 
 gcloud run deploy $app_name \
 --image $image_name \
 --region europe-north1 \
 --platform managed \
 --allow-unauthenticated \
---update-env-vars $env \
+--update-env-vars .env.yaml \
 --no-cpu-throttling \
 --min-instances 1 \
 --max-instances 1
