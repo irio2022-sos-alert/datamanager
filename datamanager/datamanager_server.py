a8ac0916d97c82088f4d3d19e22bf51c0539b222
macsyg <macieks1803@gmail.com>
2023-01-29 19:40:31 +0100

    all test 3

--- datamanager/datamanager_server.py
+++ datamanager/datamanager_server.py
@@ -1,7 +1,10 @@
 import logging
 from concurrent import futures
 
-from threading import Thread, Event
+from multiprocessing import Process
+import psutil
+import sys
+
 import time
 from datetime import datetime
 
@@ -151,61 +154,66 @@ class DataManager(datamanager_pb2_grpc.DataManagerServicer):
         return datamanager_pb2.ResponseMsg(result="service deleted")
 
 def run_in_cycle():
-    while True:
-        with Session(engine) as session:
-            stmt = select(Services)
-            services = session.exec(stmt).all()
+    try:
+        while True:
+            with Session(engine) as session:
+                stmt = select(Services)
+                services = session.exec(stmt).all()
 
-            for service in services:
-                if not (service.name in config):
-                    config[service.name] = {
-                        "service_id": service.id,
-                        "url": service.domain, 
-                        "frequency": service.frequency,
-                        "last_ping": int(round(datetime.now().timestamp())),
-                        "enabled" : True
+                for service in services:
+                    if not (service.name in config):
+                        config[service.name] = {
+                            "service_id": service.id,
+                            "url": service.domain, 
+                            "frequency": service.frequency,
+                            "last_ping": int(round(datetime.now().timestamp())),
+                            "enabled" : True
+                        }
+                    else:
+                        config[service.name] = {
+                            "service_id": service.id,
+                            "url": service.domain, 
+                            "frequency": service.frequency,
+                            "last_ping": config[service.name]["last_ping"],
+                            "enabled" : True
+                        }
+
+                services_names = [service.name for service in services]
+
+                keys = config.keys()
+                for key in keys:
+                    if key not in services_names:
+                        del config[key]
+                    
+
+            cycle_ts = int(round(datetime.now().timestamp()))
+            publisher = pubsub_v1.PublisherClient()
+            project_id = os.getenv("PROJECT_ID")
+            topic_id = os.getenv("TOPIC_ID")
+            topic_path = publisher.topic_path(project_id, topic_id)
+
+            for name in config:
+                if (config[name]["enabled"]) and (cycle_ts >= config[name]["last_ping"]):
+
+                    config[name]["last_ping"] += config[name]["frequency"]
+
+                    record = {
+                        'service_id': config[name]["service_id"],
+                        'domain': config[name]["url"]
                     }
-                else:
-                    config[service.name] = {
-                        "service_id": service.id,
-                        "url": service.domain, 
-                        "frequency": service.frequency,
-                        "last_ping": config[service.name]["last_ping"],
-                        "enabled" : True
-                    }
 
-            services_names = [service.name for service in services]
+                    data = json.dumps(record).encode("utf-8")
+                    _future = publisher.publish(topic_path, data)
+                    # logging.info(f'published message id {future.result()}')
 
-            keys = config.keys()
-            for key in keys:
-                if key not in services_names:
-                    del config[key]
-                
-
-        cycle_ts = int(round(datetime.now().timestamp()))
-        publisher = pubsub_v1.PublisherClient()
-        project_id = os.getenv("PROJECT_ID")
-        topic_id = os.getenv("TOPIC_ID")
-        topic_path = publisher.topic_path(project_id, topic_id)
-
-        for name in config:
-            if (config[name]["enabled"]) and (cycle_ts >= config[name]["last_ping"]):
-
-                config[name]["last_ping"] += config[name]["frequency"]
-
-                record = {
-                    'service_id': config[name]["service_id"],
-                    'domain': config[name]["url"]
-                }
-
-                data = json.dumps(record).encode("utf-8")
-                _future = publisher.publish(topic_path, data)
-                # logging.info(f'published message id {future.result()}')
-
-                logging.info("HELLO FROM %s", name)
+                    logging.info("HELLO FROM %s", name)
 
 
-        time.sleep(0.05)
+            time.sleep(0.05)
+    except:
+        p = psutil.Process(os.getppid())
+        p.terminate()
+        sys.exit()
 
 def init_db():
     global engine
@@ -219,8 +227,8 @@ def serve(port) -> None:
     global config
     config = {}
 
-    thread = Thread(target=run_in_cycle)
-    thread.start()
+    proc = Process(target=run_in_cycle)
+    proc.start()
 
     bind_address = f"[::]:{port}"
     server = grpc.server(futures.ThreadPoolExecutor())
