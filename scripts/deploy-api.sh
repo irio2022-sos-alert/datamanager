648dcc00877ae23a194b3518a15d192bcf5298bf
macsyg <macieks1803@gmail.com>
2023-01-29 12:18:26 +0100

    api test 2

--- scripts/deploy-api.sh
+++ scripts/deploy-api.sh
@@ -2,15 +2,10 @@
 
 readonly app_name="$1"
 readonly image_name="$2"
-readonly cloudsql_instance="$3"
 
 gcloud run deploy $app_name \
 --image $image_name \
 --region europe-north1 \
 --platform managed \
 --allow-unauthenticated \
---min-instances 1 \
---max-instances 1 \
 --env-vars-file .env.yaml \
---add-cloudsql-instances $cloudsql_instance \
---no-cpu-throttling \
